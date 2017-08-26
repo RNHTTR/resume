d58eb9ae4d4ca413c80c485450dd9310b644334a
RNHTTR <ryan@wiftapp.com>
2017-08-26 15:55:57 -0400

    20170826 update

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,51 +1,78 @@
 var name = "Ryan Hatter";
-var role = "Aspiring Full-Stack Developer";
+var role = "";
 var phone = "813-326-8778";
-var email = "Ryan@WiftApp.com";
+var email = "Ryan.N.Hatter@Gmail.com";
 var photoPath = "professional-image.jpg";
 var city = "Tampa, FL";
-var skills = [
+var proficientSkills = [
     //Tech
-    "Swift",
     "Python",
-    "Firebase",
+    "Pandas",
+    "Swift",
+    "Google Apps Scripts",
+    // "Firebase",
     // "Google App Engine",
-    "HTML/CSS",
-    "JavaScript",
-    "VBA",
-    "Advanced Microsoft Excel",
+    // "HTML/CSS",
+    // "JavaScript",
+    "Microsoft Excel",
+    "VBA"
     //Other
-    "Project Management",
-    "Verbal & Written Communication",
-    "Creative Writing"
+    // "Project Management",
+    // "Verbal & Written Communication",
+    // "Creative Writing"
 ];
+
+var workingSkills = [
+    // "Pandas",
+    "NumPy",
+    "SQL",
+    "Google App Engine",
+    "AWS",
+    "Firebase",
+    "HTML/CSS/JS",
+    "React"
+];
+
 var hobbies = [
-    "traveling",
-    "sports",
-    "rock climbing",
-    "reading"
+    "Traveling",
+    "Sports",
+    "Rock climbing",
+    "Reading",
+    "Yoga"
 ];
 
 var bio = {
-    "name" : name,
-    "role" : role,
-    "phone" : phone,
-    "email" : email,
-    "photo" : photoPath,
-    "location" : city,
-    "skills" : skills
+    "name":          name,
+    "role":          role,
+    "phone":         phone,
+    "email":         email,
+    "photo":         photoPath,
+    "location":      city,
+    "skills":        proficientSkills,
+    "workingSkills": workingSkills
 };
 
 var work = {
     "jobs" : [
+         {
+            "employer":         "Nielsen",
+            "title":            "Software Developer",
+            "dates":            "May 2017 - Present",
+            "location":         "Tampa, FL",
+            "responsibilities": ["Port data fusion applications from SQL to Python. Integrate updated Python modules in the cloud via Amazon Web Services using the Boto3 SDK.",
+                                  "Without being prompted by management, ported outdated Excel timesheet system to Google Sheets after Nielsen transitioned from Microsoft Office to G Suite. The new system uses Google Apps Scripts to store create and save timesheet backups and provides functionality for users to easily submit their timesheet to their manager. The system was adopted for all interns for 2017."
+                                 ],
+            "link":             "https://www.nielsen.com"
+        },
         {
-            "employer" : "Milliman, Inc.",
-            "title" : "Actuarial Analyst",
-            "dates" : "May 2015 - March 2017",
-            "location" : "Tampa, FL",
-            "responsibilities" : ["Developed VBA macros to reduce monthly valuation process from 30 hours per month to fewer than seven hours per month",
-            "Created team that developed and implemented week-long training program for new hires"
-            ]
+            "employer":         "Milliman, Inc.",
+            "title":            "Actuarial Analyst",
+            "dates":            "May 2015 - March 2017",
+            "location":         "Tampa, FL",
+            "responsibilities": ["Developed VBA macros to reduce monthly valuation process from 30 hours per month to fewer than seven hours per month",
+                                  "Created team that developed and implemented week-long training program for new hires"
+                                 ],
+            "link":              "https://www.milliman.com"
         }
     ]
 };
@@ -53,15 +80,28 @@ var work = {
 var projects = {
     "projects" : [
         {
-            "name" : "Wift",
-            "date" : "Summer 2016 - Present",
-            "description" : "Wift is a mobile (iOS) food-ordering platform. I singlehandedly (albeit with the help of Team Treehouse courses and Stack Overflow) wrote the client in Swift and added custom server-side logic in Python to accomodate a payment gateway and device-to-device push notifications. Data is stored in Firebase and the server is hosted on the Google Cloud Platform."
+            "name":        "Excellent Actuary",
+            "date":        "2017",
+            "description": "Excellent Actuary provides basic training in Microsoft Excel while introducing users to basic actuarial & insurance knowledge. The website is currently front-end only and utilizes HTML, CSS, and JavaScript.",
+            "link":        "http://www.excellentactuary.com"
         },
-
         {
-            "name" : "Excellent Actuary",
-            "date" : "2017",
-            "description" : "Excellent Actuary will provide basic training in Microsoft Excel while introducing users to actuarial & insurance knowledge. The front-end will be developed in HTML/CSS/JavaScript and the back-end will be developed with Python and Flask and will be hosted on the Google Cloud Platform."
+            "name":        "Wift",
+            "date":        "Summer 2016 - Summer 2017",
+            "description": "Wift was a mobile food-ordering platform. I developed the client in Swift and added custom server-side logic in Python/Flask to accomodate the Braintree payment gateway and device-to-device push notifications. Data is stored in Firebase and the server is hosted on the Google Cloud Platform. Wift achieved a beta release with an agreement with Bayside Cafe in Tampa, Florida, but has been put on hold.",
+            "link":        "http://www.wiftapp.com"
+        },
+        {
+            "name":        "SwiftyFont",
+            "date":        "2017",
+            "description": "SwiftyFont is an iOS application written in Swift that allows iOS developers see what each available Swift font looks like on a variety of background colors.",
+            "link":        "https://SwiftyFont.wordpress.com"
+        },
+        {
+            "name":        "Google Apps Script Meetup Presentation",
+            "date":        "September 5, 2017",
+            "description": "I will give a presentation on how to use Google Apps Scripts to enhance Google Sheets using SpreadsheetApp, DriveApp, and MailApp. A video walkthrough of the project can be found at the link in the title of this project.",
+            "link":        "http://www.ryanhatter.me/meetup"
         }
     ]
 }
@@ -69,43 +109,49 @@ var projects = {
 var education = {
     "schools" : [
         {
-            "name" : "University of South Florida",
-            "city" : "Tampa, FL",
-            "gpa" : "3.6",
-            "majors" : ["Statistics"],
-            "dates" : "Graduated: 2015"
+            "name":        "University of South Florida",
+            "city":        "Tampa, FL",
+            "gpa":         "3.6",
+            "involvement": "Actuarial Society - Treasurer",
+            "majors":      ["Statistics"],
+            "dates":       "Graduated: 2015",
+            "link":        "http://www.usf.edu"
         }
     ],
     "onlineCourses" : [
         {
-            "title" : "Swift",
-            "school" : "Treehouse",
-            "date" : 2016,
-            "url" : "http://www.teamtreehouse.com"
+            "title":  "Intro to Data Analysis",
+            "school": "Udacity",
+            "date":   2017,
+            "url":    "https://www.udacity.com/course/intro-to-data-analysis--ud170"
         },
         {
-            "title" : "Python, Flask",
-            "school" : "Treehouse",
-            "date" : 2016,
-            "url" : "http://www.teamtreehouse.com"
+            "title":  "Intro to Hadoop and MapReduce",
+            "school": "Udacity",
+            "date":   2017,
+            "url":    "https://www.udacity.com/course/intro-to-hadoop-and-mapreduce--ud617"
         },
         {
-            "title" : "Web Development",
-            "school" : "Udacity",
-            "date" : 2017,
-            "url" : "http://www.udacity.com"
+            "title":  "Python, Flask",
+            "school": "Treehouse",
+            "date":   2016,
+            "url":    "https://teamtreehouse.com/tracks/learn-flask"
+        },
+        {
+            "title":  "Swift",
+            "school": "Treehouse",
+            "date":   2016,
+            "url":    "https://teamtreehouse.com/tracks/ios-development-with-swift-20"
         }
     ]
 };
 
-// var formattedContact = HTMLcontactGeneric.replace("%data%", "Contact info:")
-// formattedContact = formattedContact.replace
-var formattedName = HTMLheaderName.replace("%data%", bio.name);
-var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-var formattedEmail = HTMLemail.replace("%data%", bio.email);
-var formattedPhone = HTMLmobile.replace("%data%", bio.phone);
+var formattedName     = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole     = HTMLheaderRole.replace("%data%", bio.role);
+var formattedEmail    = HTMLemail.replace("%data%", bio.email);
+var formattedPhone    = HTMLmobile.replace("%data%", bio.phone);
 var formattedLocation = HTMLlocation.replace("%data%", bio.location);
-var formattedPicture = HTMLbioPic
+var formattedPicture  = HTMLbioPic
 
 var formattedBio = [formattedName,
                     formattedRole
@@ -116,16 +162,11 @@ var details = [formattedEmail,
                  formattedLocation
                 ];
 
-// $("#header").append(formattedPicture);
-
 for (var item in formattedBio) {
     $("#header").append(formattedBio[item]);
 }
 
-// $("#header").append(formattedPicture);
-
 for (var item in details) {
-    // $("#header").append(formattedContact);
     $("#header").append(details[item]);
 }
 
@@ -134,33 +175,48 @@ $("#header").append(formattedPicture);
 if (bio.skills.length > 0) {
     $("#header").append(HTMLskillsStart);
 
-    var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
-    $("#skills").append(formattedSkill);
-    formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
-    $("#skills").append(formattedSkill);
-    formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
-    $("#skills").append(formattedSkill);
-    formattedSkill = HTMLskills.replace("%data%", bio.skills[3]);
-    $("#skills").append(formattedSkill);
-    formattedSkill = HTMLskills.replace("%data%", bio.skills[4]);
-    $("#skills").append(formattedSkill);
-    formattedSkill = HTMLskills.replace("%data%", bio.skills[5]);
-    $("#skills").append(formattedSkill);
-    formattedSkill = HTMLskills.replace("%data%", bio.skills[6]);
-    // $("#skills").append(formattedSkill);
-    // formattedSkill = HTMLskills.replace("%data%", bio.skills[7]);
-}
+    var formattedProficientSkill = HTMLproficientSkills
+    $("#skills").append(formattedProficientSkill);
 
-// $("#header").append(formattedPicture);
+    for (i = 0; i < proficientSkills.length; i++) {
+        var formattedSkill = HTMLskills.replace("%data%", bio.skills[i]);
+        $("#skills").append(formattedSkill);
+    }
+
+    var newDiv = document.createElement('div');
+    newDiv.setAttribute('id', 'work-skills-start');
+    document.getElementById('header').append(newDiv);
+
+    var formattedWorkingSkill = HTMLworkingSkills;
+    $("#work-skills-start").append(formattedWorkingSkill);
+
+    for (i = workingSkills.length - 1; i >= 0; i--) {
+        var formattedSkill = HTMLworkingSkill.replace("%data%", bio.workingSkills[i]);
+        $("#working-skills").after(formattedSkill);
+    }
+
+    $("#header").append(HTMLhobbiesStart);
+
+    var hobbiesDiv = document.createElement('div');
+    hobbiesDiv.setAttribute('id', 'hobbies-start');
+    document.getElementById('header').append(hobbiesDiv);
+
+    for (i = 0; i < hobbies.length; i++) {
+        var formattedSkill = HTMLskills.replace("%data%", hobbies[i]);
+        $("#hobbies-start").append(formattedSkill);
+    }
+
+}
 
 for (job in work.jobs) {
     $("#workExperience").append(HTMLworkStart);
 
-    var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
-    var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+    var formattedEmployer      = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+    formattedEmployer          = formattedEmployer.replace('%#%', work.jobs[job].link);
+    var formattedTitle         = HTMLworkTitle.replace("%data%", work.jobs[job].title);
     var formattedEmployerTitle = formattedEmployer + formattedTitle;
-    var formattedWorkDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
-    var formattedWorkLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+    var formattedWorkDates     = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+    var formattedWorkLocation  = HTMLworkLocation.replace("%data%", work.jobs[job].location);
 
     $(".work-entry:last").append(formattedEmployerTitle, formattedWorkDates, formattedWorkLocation);
 
@@ -174,8 +230,9 @@ for (job in work.jobs) {
 for (project in projects.projects) {
     $("#projects").append(HTMLprojectStart);
 
-    var formattedProjectName = HTMLprojectTitle.replace("%data%", projects.projects[project].name);
-    var formattedProjectDate = HTMLprojectDates.replace("%data%", projects.projects[project].date);
+    var formattedProjectName        = HTMLprojectTitle.replace("%data%", projects.projects[project].name);
+    formattedProjectName            = formattedProjectName.replace("%#%", projects.projects[project].link);
+    var formattedProjectDate        = HTMLprojectDates.replace("%data%", projects.projects[project].date);
     var formattedProjectDescription = HTMLprojectDescription.replace("%data%", "- " + projects.projects[project].description);
 
     $(".project-entry:last").append(formattedProjectName, formattedProjectDate, formattedProjectDescription);
@@ -186,14 +243,15 @@ $("#education").append(HTMLcollege);
 for (school in education.schools) {
     $("#education").append(HTMLschoolStart);
 
-    var formattedSchool = HTMLschoolName.replace("%data%", education.schools[school].name);
-    var formattedGraduation = HTMLschoolDates.replace("%data%", education.schools[school].dates);
-    var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools[school].city);
-    var formattedMajor = HTMLschoolMajor.replace("%data%", education.schools[school].majors);
-    var formattedGPA = HTMLschoolGPA.replace("%data%", education.schools[school].gpa);
+    var formattedSchool      = HTMLschoolName.replace("%data%", education.schools[school].name);
+    formattedSchool          = formattedSchool.replace("%#%", education.schools[school].link);
+    var formattedGraduation  = HTMLschoolDates.replace("%data%", education.schools[school].dates);
+    var formattedLocation    = HTMLschoolLocation.replace("%data%", education.schools[school].city);
+    var formattedMajor       = HTMLschoolMajor.replace("%data%", education.schools[school].majors);
+    var formattedGPA         = HTMLschoolGPA.replace("%data%", education.schools[school].gpa);
+    var formattedInvolvement = HTMLschoolInvolvement.replace("%data%", education.schools[school].involvement);
 
-    $(".education-entry:last").append(formattedSchool, formattedGraduation, formattedLocation, formattedMajor, formattedGPA);
-    // $(".education-entry:last").append(formattedSchool, formattedGraduation, formattedMajor, formattedGPA);
+    $(".education-entry:last").append(formattedSchool, formattedGraduation, formattedLocation, formattedMajor, formattedGPA, formattedInvolvement);
 };
 
 $("#education").append(HTMLonlineClasses);
@@ -202,10 +260,10 @@ for (course in education.onlineCourses) {
     $("#education").append(HTMLschoolStart);
 
     var formattedOnlineCourse = HTMLonlineTitle.replace("%data%", education.onlineCourses[course].title);
+    formattedOnlineCourse     = formattedOnlineCourse.replace("#", education.onlineCourses[course].url);
     var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[course].school);
-    var formattedOnlineDate = HTMLonlineDates.replace("%data%", education.onlineCourses[course].date);
+    var formattedOnlineDate   = HTMLonlineDates.replace("%data%", education.onlineCourses[course].date);
     var formattedCourseSchool = formattedOnlineCourse + formattedOnlineSchool;
-    var formattedURL = HTMLonlineURL.replace("%data%", education.onlineCourses[course].url)
 
     $(".education-entry:last").append(formattedCourseSchool, formattedOnlineDate);
 }
